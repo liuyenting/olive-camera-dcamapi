7d607ce348eeb0adaf992bb9ca3f044327a6640d
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-08-06 13:47:46 +0800

    Fix namespace package path

--- setup.py
+++ setup.py
@@ -1,42 +1,100 @@
+import os
 from setuptools import setup, Extension
-from Cython.Distutils import build_ext
+
+from Cython.Build import build_ext
 
 NAME = "olive-camera-dcamapi"
 VERSION = "0.1"
-DESCR = "A small template project that shows how to wrap C/C++ code into python using Cython"
-URL = "http://www.google.com"
-REQUIRES = ['numpy', 'cython']
+DESCRIPTION = "A small template project that shows how to wrap C/C++ code into python using Cython"
+URL = 'https://github.com/liuyenting/olive-camera-dcamapi'
+
+# Trove classifiers
+#   https://pypi.org/classifiers/
+CLASSIFIERS = [
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: Microsoft :: Windows'
+]
+KEYWORDS = []
 
 AUTHOR = "Liu, Yen-Ting"
 EMAIL = "ytliu@gate.sinica.edu.tw"
 
-LICENSE = "Apache 2.0"
+REQUIRES = []
 
-SRC_DIR = "olive-camera-dcamapi"
-PACKAGES = [SRC_DIR]
+PACKAGES=[
+    'olive.devices.drivers.dcamapi'
+]
 
-ext_1 = Extension(
-    "dcamapi",      
-    [SRC_DIR + "/dcamapi4.pyx"],
-    language='c',
-    include_dirs=[],
-    extra_objects=[SRC_DIR+'/lib/dcamapi.lib']
+EXT_DEFS = [
+    {
+        'name': 'olive.devices.drivers.dcamapi.dcamapi4',
+        'extra_objects': [
+            'lib/dcamapi.lib'
+        ]
+    }
+]
+
+######################################################################################
+
+cwd = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(cwd, "README.md"), encoding='utf-8') as fd:
+    LONG_DESCRIPTION = fd.read()
+
+# - install cython headers so other modules can cimport
+# - force sdist to keep the .pyx files
+PACKAGE_DATA = {pkg: ['*.pxd', '*.pyx'] for pkg in PACKAGES}
+
+# generate extension constructors
+def generate_extension(ext_def):
+    assert ('name' in ext_def), 'invalid extension name'
+
+    ext_path = ext_def['name'].replace('.', os.path.sep) + '.pyx'
+    ext_root = os.path.dirname(ext_path)
+
+    ext_def['sources'] = [ext_path]
+
+    # prepend root directory
+    arguments = ('include_dirs', 'library_dirs', 'runtime_library_dirs', 'extra_objects')
+    for argument in arguments:
+        try:
+            ext_def[argument] = [os.path.join(ext_root, path) for path in ext_def[argument]]
+        except KeyError:
+            # ignore unused argument
+            pass
+
+    return Extension(**ext_def)
+
+EXTENSIONS = [generate_extension(ext_def) for ext_def in EXT_DEFS]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    url=URL,
+
+    classifiers=CLASSIFIERS,
+    keywords=KEYWORDS,
+
+    author=AUTHOR,
+    author_email=EMAIL,
+
+    # use pyproject.toml for setup dependencies instead
+    #setup_requires=[],
+    install_requires=REQUIRES,
+
+    # package to install
+    packages=PACKAGES,
+    namespace_packages=['olive.devices.drivers'],
+    package_data=PACKAGE_DATA,
+    ext_modules=EXTENSIONS,
+
+    cmdclass={"build_ext": build_ext},
+
+    # disable zip_safe
+    #   - Cython cannot find .pxd files inside installed .egg
+    #   - dynmaic loader may require library unzipped to a temporary directory at import time
+    zip_safe=False
 )
-
-EXTENSIONS = [ext_1]
-
-if __name__ == "__main__":
-    setup(
-        install_requires=REQUIRES,
-        packages=PACKAGES,
-        zip_safe=False,
-        name=NAME,
-        version=VERSION,
-        description=DESCR,
-        author=AUTHOR,
-        author_email=EMAIL,
-        url=URL,
-        license=LICENSE,
-        cmdclass={"build_ext": build_ext},
-        ext_modules=EXTENSIONS
-    )
