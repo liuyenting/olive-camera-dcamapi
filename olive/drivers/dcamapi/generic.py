453b384dd951869d6e64e62a925f1112d86492cd
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-08-16 18:50:18 +0800

    Dummy push

--- olive/drivers/dcamapi/generic.py
+++ olive/drivers/dcamapi/generic.py
@@ -10,19 +10,19 @@ from .dcamapi import DCAM_IDSTR
 class GenericCamera(Camera):
     def __init__(self):
         super().__init__()
-        self._camera = DCAMAPI()
+        self._handle = DCAMAPI()
 
     # High level functions
     def enumerate_cameras(self):
         cameras = []
-        for i in range(self.camera.n_devices):
+        for i in range(self.handle.n_devices):
             try:
-                self.camera.open(i)
-                self.camera.close()
+                self.handle.open(i)
+                self.handle.close()
             except RuntimeError:
                 continue
             
-            get_info = partial(self.camera.get_string, index=i)
+            get_info = partial(self.handle.get_string, index=i)
 
             raw_sn = get_info(DCAM_IDSTR.DCAM_IDSTR_CAMERAID)
             params = {
@@ -37,5 +37,5 @@ class GenericCamera(Camera):
     # Low level functions
 
     @property
-    def camera(self):
-        return self._camera
+    def handle(self):
+        return self._handle
